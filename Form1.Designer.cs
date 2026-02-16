2c51e703dc24aefa697c32ee30cbd2ea311df090
jatuporn-cmd <jatuporn.pimp@sbacnon.ac.th>
2026-02-16 09:37:07 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace HomeworkReminder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblID = new System.Windows.Forms.Label();
+            this.txtHomeworkID = new System.Windows.Forms.TextBox();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.txtHomeworkTitle = new System.Windows.Forms.TextBox();
+            this.lblSubject = new System.Windows.Forms.Label();
+            this.cboSubject = new System.Windows.Forms.ComboBox();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.cboStatus = new System.Windows.Forms.ComboBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblID
+            // 
+            this.lblID.AutoSize = true;
+            this.lblID.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblID.Location = new System.Drawing.Point(50, 50);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(94, 20);
+            this.lblID.TabIndex = 0;
+            this.lblID.Text = "รหัสการบ้าน :";
+            // 
+            // txtHomeworkID
+            // 
+            this.txtHomeworkID.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtHomeworkID.Location = new System.Drawing.Point(200, 47);
+            this.txtHomeworkID.Name = "txtHomeworkID";
+            this.txtHomeworkID.Size = new System.Drawing.Size(200, 26);
+            this.txtHomeworkID.TabIndex = 1;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(50, 100);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(107, 20);
+            this.lblTitle.TabIndex = 2;
+            this.lblTitle.Text = "ชื่อเรื่องการบ้าน :";
+            // 
+            // txtHomeworkTitle
+            // 
+            this.txtHomeworkTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtHomeworkTitle.Location = new System.Drawing.Point(200, 97);
+            this.txtHomeworkTitle.Name = "txtHomeworkTitle";
+            this.txtHomeworkTitle.Size = new System.Drawing.Size(400, 26);
+            this.txtHomeworkTitle.TabIndex = 3;
+            // 
+            // lblSubject
+            // 
+            this.lblSubject.AutoSize = true;
+            this.lblSubject.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSubject.Location = new System.Drawing.Point(50, 150);
+            this.lblSubject.Name = "lblSubject";
+            this.lblSubject.Size = new System.Drawing.Size(60, 20);
+            this.lblSubject.TabIndex = 4;
+            this.lblSubject.Text = "รายวิชา :";
+            // 
+            // cboSubject
+            // 
+            this.cboSubject.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboSubject.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboSubject.FormattingEnabled = true;
+            this.cboSubject.Items.AddRange(new object[] {
+            "คณิตศาสตร์",
+            "วิทยาศาสตร์",
+            "ภาษาอังกฤษ",
+            "สังคมศึกษา",
+            "คอมพิวเตอร์"});
+            this.cboSubject.Location = new System.Drawing.Point(200, 147);
+            this.cboSubject.Name = "cboSubject";
+            this.cboSubject.Size = new System.Drawing.Size(200, 28);
+            this.cboSubject.TabIndex = 5;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.Location = new System.Drawing.Point(50, 200);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(57, 20);
+            this.lblStatus.TabIndex = 6;
+            this.lblStatus.Text = "สถานะ :";
+            // 
+            // cboStatus
+            // 
+            this.cboStatus.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboStatus.FormattingEnabled = true;
+            this.cboStatus.Items.AddRange(new object[] {
+            "Not Started",
+            "Doing",
+            "Finished"});
+            this.cboStatus.Location = new System.Drawing.Point(200, 197);
+            this.cboStatus.Name = "cboStatus";
+            this.cboStatus.Size = new System.Drawing.Size(200, 28);
+            this.cboStatus.TabIndex = 7;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.LightGreen;
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(200, 250);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(120, 40);
+            this.btnAdd.TabIndex = 8;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.cboStatus);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.cboSubject);
+            this.Controls.Add(this.lblSubject);
+            this.Controls.Add(this.txtHomeworkTitle);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.txtHomeworkID);
+            this.Controls.Add(this.lblID);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "โปรแกรมบันทึกการบ้าน";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblID;
+        private System.Windows.Forms.TextBox txtHomeworkID;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.TextBox txtHomeworkTitle;
+        private System.Windows.Forms.Label lblSubject;
+        private System.Windows.Forms.ComboBox cboSubject;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.ComboBox cboStatus;
+        private System.Windows.Forms.Button btnAdd;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace HomeworkReminder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            // คำสั่งนี้จะไปเรียกโค้ดใน Form1.Designer.cs ให้ทำงาน
+            InitializeComponent();
+
+            // ตั้งค่าเริ่มต้นให้ ComboBox เลือกตัวแรกสุด
+            if (cboSubject.Items.Count > 0) cboSubject.SelectedIndex = 0;
+            if (cboStatus.Items.Count > 0) cboStatus.SelectedIndex = 0;
+        }
+
+        // ฟังก์ชันเมื่อกดปุ่ม Add (เชื่อมกับปุ่มใน Designer แล้ว)
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            // 1. รับค่า
+            string hwId = txtHomeworkID.Text.Trim();
+            string hwTitle = txtHomeworkTitle.Text.Trim();
+            string subject = cboSubject.Text;
+            string status = cboStatus.Text;
+
+            // 2. Data Validation
+            if (string.IsNullOrEmpty(hwId) || string.IsNullOrEmpty(hwTitle))
+            {
+                MessageBox.Show("กรุณากรอกข้อมูลให้ครบถ้วน!\n(ห้ามเว้นว่าง รหัสการบ้าน และ ชื่อเรื่อง)",
+                                "แจ้งเตือน",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Warning);
+                return;
+            }
+
+            // 3. แสดงผล (MessageBox)
+            string message = "บันทึกข้อมูลสำเร็จ!\n\n" +
+                             "รหัสการบ้าน: " + hwId + "\n" +
+                             "ชื่อเรื่อง: " + hwTitle + "\n" +
+                             "รายวิชา: " + subject + "\n" +
+                             "สถานะ: " + status;
+
+            MessageBox.Show(message,
+                            "รายละเอียดการบ้าน",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Information);
+
+            // 4. ล้างหน้าจอ
+            txtHomeworkID.Clear();
+            txtHomeworkTitle.Clear();
+            cboSubject.SelectedIndex = 0;
+            cboStatus.SelectedIndex = 0;
+            txtHomeworkID.Focus();
+        }
+    }
+}
